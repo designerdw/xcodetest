efea161bd93689e99cdd7ba460c48d4da53288b4
박대웅 <designerdw@h3xb0x.local>
2016-02-24 02:29:30 +0900

    b2

--- testtesttest/main.cpp
+++ testtesttest/main.cpp
@@ -11,6 +11,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout << "b1" << std::endl;
+    std::cout << "b2" << std::endl;
     return 0;
 }
